b0f9248b4cdb321ddaf7a9edda81bd94896d67de
Kirk Musngi <musngikd@gmail.com>
2014-03-09 15:21:16 -0400

    Change structure of demo

--- README.md
+++
@@ -1,4 +0,0 @@
-PHAHackathon
-============
-
-PHA Hackathon - Online platform for teachers to facilitate cooking channel for classroom
--- app/models/teacher.rb
+++
@@ -1,6 +0,0 @@
-class Teacher < ActiveRecord::Base
-  before_save { self.email = email.downcase }
-  	
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true
-end
